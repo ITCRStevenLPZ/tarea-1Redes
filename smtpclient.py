45531832db43a736b5228e76b498cbd6ce3981a3
ITCRStevenLPZ <esquivellopezronald@hotmail.com>
2021-09-07 15:50:24 -0600

    [+]GUI added succesfully  to the smtpclient

--- smtpclient.py
+++ smtpclient.py
@@ -31,7 +31,7 @@ def send(message, subject, sender, recipients, host, port):
 
     reactor.run()
 
-class MailClientOptions(Options):
+'''class MailClientOptions(Options):
     synopsis = "smptclient.py [options]"
 
     optParameters = [
@@ -75,7 +75,7 @@ class MailClientOptions(Options):
         try:
             testing = open(self["message"],'r')
         except Exception as e:
-            raise UsageError(e)
+            raise UsageError(e)'''
 
 def GetRecipients(addARR):
     awsr = []
@@ -85,7 +85,7 @@ def GetRecipients(addARR):
 
 
 
-def main(args=None):
+'''def main(args=None):
 
     import sys
     o = MailClientOptions()
@@ -114,7 +114,7 @@ def main(args=None):
     subject = str(msgList[0][0])
 
     body = msgList[1:]
-    msg = '\n'.join(''.join(elems) for elems in body)
+    msg = ''.join(''.join(elems) for elems in body)
     host = o["serverhost"].split(sep=":")[0]
     port = int(o["serverhost"].split(sep=":")[1])
     sender = "test@example.com"
@@ -122,5 +122,66 @@ def main(args=None):
     log.startLogging(sys.stdout)
     send(msg, subject, sender, recipients, host, port)
 
+
+
+
+
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main(sys.argv[1:])'''
+
+from tkinter import *
+
+root = Tk()
+        
+root.configure(background='white')
+
+root.geometry("500x600")
+
+root.title('Send Mail')
+
+label_0 =Label(root,text="Ronald's SMTP Client", width=40,font=("bold",20))
+label_0.place(x=0,y=60)
+
+label_1 =Label(root,text="From", width=10,font=("bold",10))
+label_1.place(x=0,y=130)
+
+from_entry=Entry(root, width=50)
+from_entry.place(x=100,y=130)
+
+label_2 =Label(root,text="To", width=10,font=("bold",10))
+label_2.place(x=0,y=180)
+
+to_entry=Entry(root, width=50)
+to_entry.place(x=100,y=180)
+
+label_3 =Label(root,text="Subject", width=10,font=("bold",10))
+label_3.place(x=0,y=230)
+
+subject_entry=Entry(root, width=50)
+subject_entry.place(x=100,y=230)
+
+label_4 =Label(root,text="Body", width=10,font=("bold",10))
+label_4.place(x=0,y=280)
+
+body_entry=Text(root, width=50, height=20)
+body_entry.place(x=100,y=280)
+
+def retrieve_body():
+    input = body_entry.get("1.0",END)
+    return input
+
+def buttonClick():
+    subject = subject_entry.get()
+    msg = retrieve_body()
+    host = "localhost"
+    port = 2500
+    sender = from_entry.get()
+    recipients = to_entry.get().split(sep=', ')
+    log.startLogging(sys.stdout)
+    send(msg, subject, sender, recipients, host, port)
+
+
+submit = Button(root, text='Send Mail' , command = buttonClick, width=20,bg="black",fg='white').place(x=180,y=560)
+
+
+root.mainloop()
--- tests.py
+++
@@ -1,6 +0,0 @@
-with open('Inbox/passwords.txt', "rb") as f:
-    for line in f:
-        line = line.rstrip()
-        print(line)
-        parts = line.split(b":")
-        print(parts)
